82d2a0002f3f06f1246b2ec0eaa4ef869026957f
Hsinyao <hsinyao009@gmail.com>
2022-02-14 00:09:14 +0800

    install eslint loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./style.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
 
-console.log("I am printing logs");
-
-ReactDOM.render(<div>Hello World</div>, document.getElementById("root"));
+ReactDOM.render(<div>Hello World</div>, document.getElementById('root'));
